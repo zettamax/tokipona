e1f762f12de52ab7026eb2bb147d90c97a6f290d
max <zettamax@gmail.com>
2018-04-25 21:27:47 +0300

    Cache Word components (don't sure it's a good idea)

--- src/components/Wordlist/Wordlist.js
+++ src/components/Wordlist/Wordlist.js
@@ -5,18 +5,27 @@ import Word from '../Word/Word';
 import words from '../../tokipona';
 
 class Wordlist extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.words = words.map(word => word.toki);
+        this.renderedWords = {};
+
+        words.forEach(word =>
+            this.renderedWords[word.toki] =
+                <Word toki={word.toki} image={word.image}
+                      en={word.en} ru={word.ru} it={word.it} key={word.toki}/>
+        );
+    }
+
     render() {
+        const filtered = this.props.query.length ?
+            this.words.filter(word => word.includes(this.props.query)) :
+            this.words;
+
         return (
             <Row>
-                {
-                    words
-                        .filter(
-                            (word) => word.toki.includes(this.props.query))
-                        .map(
-                            (word) => <Word toki={word.toki} image={word.image}
-                                        en={word.en} ru={word.ru} it={word.it} key={word.toki} />
-                    )
-                }
+                { filtered.map(word => this.renderedWords[word]) }
             </Row>
         )
     }
